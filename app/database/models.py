be1f0c5539d04c53c73b14d5bcd83670e577c629
Anastasiia Pokasova <pokasovaai@gmail.com>
2022-11-25 22:06:55 +0300

    #5 add models mapping

--- app/database/models.py
+++ app/database/models.py
@@ -1,56 +1,53 @@
-import sqlalchemy
+import sqlalchemy as sa
 
-with sqlalchemy.create_engine(
-        'postgresql:///postgres',
-        isolation_level='AUTOCOMMIT'
-).connect() as connection:
-    connection.execute("""create role dbowner with
-                          createdb
-                          login
-                          password 'temppass';"""
-                       )
-    connection.execute(""" create database questionsdb
-                           owner dbowner;"""
-                       )
-    connection.execute(
-        """
-        create table literature (
-        id serial,
-        title varchar(256) not null,
-        author varchar(256) not null
-    )
-    
-    create table questions (
-        id bigserial,
-        question varchar not null,
-        answer varchar,
-        literature_id references literature(id)
-    )
-    
-    create table jobs (
-        id serial,
-        title varchar(256) not null,
-        definition varchar(256)
-    )
-    
-    create table companies (
-        id serial,
-        title varchar(256) not null,
-        definition varchar(256),
-        attitude numeric(1, 1)
-    )
-    
-    create table keywords (
-        id serial
-        keyword text not null
-    )
-    
-    create table unification (
-        id bigserial,
-        job references jobs(id),
-        company references companies(id),
-        keyword references keywords(id),
-        question references questions(id)
-    )
-        """
-    )
+from app.database.database import Base
+
+
+class Literature(Base):
+    __tablename__ = 'literature'
+
+    id = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
+    title = sa.Column(sa.String)
+    author = sa.Column(sa.String)
+
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
+    question = sa.Column(sa.Text)
+    answer = sa.Column(sa.Text, nullable=True)
+    literature_id = sa.Column(sa.BigInteger, sa.ForeignKey('literature.id'))
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    title = sa.Column(sa.String)
+    definition = sa.Column(sa.String, nullable=True)
+    attitude = sa.Column(sa.types.Numeric(1, 1), nullable=True)
+
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    title = sa.Column(sa.String(256))
+    definition = sa.Column(sa.Text, nullable=True)
+
+
+class Keyword(Base):
+    __tablename__ = "keywords"
+    id = sa.Column(sa.Integer, primary_key=True)
+    keyword = sa.Column(sa.Text)
+
+
+class Unification(Base):
+    __tablename__ = "unification"
+
+    id = sa.Column(sa.BigInteger, primary_key=True)
+    job = sa.Column(sa.Integer, sa.ForeignKey("jobs.id"))
+    company = sa.Column(sa.Integer, sa.ForeignKey("companies.id"))
+    keywords = sa.Column(sa.Integer, sa.ForeignKey("keywords.id"))
+    questions = sa.Column(sa.BigInteger, sa.ForeignKey("questions.id"))
